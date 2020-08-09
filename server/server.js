85e7185693233e1b968877331715f7a88c06639c
bong8230 <50412254+bong8230@users.noreply.github.com>
2020-08-09 12:18:37 +0900

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,34 +1,46 @@
+//require('dotenv').config();
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
+const http = require("http");
 const app = express();
-const db = require("./app/models");
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
+const server = http.createServer(app);
+const socket = require("socket.io")
+const io = socket(server);
+const db = require('./DB.js');
+const rooms = {};
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
+io.on('connection', socket => {
+    socket.on("join room", roomID => {
+        if (rooms[roomID]) {
+            rooms[roomID].push(socket.id);
+        } else {
+            rooms[roomID] = [socket.id];
+        }
+        const otherUser = rooms[roomID].find(id => id !== socket.id);
 
-app.use(cors(corsOptions));
+        if (otherUser) {
+            socket.emit("other user", otherUser);
+            socket.to(otherUser).emit("user joined", socket.id);
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
+        }
+    });
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+    socket.on("offer", payload => {
+        io.to(payload.target).emit("offer", payload);
+    });
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+    socket.on("answer", payload => {
+        io.to(payload.target).emit("answer", payload);
+    });
+
+    socket.on("ice-candidate", incoming => {
+        io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+    });
+
 });
 
-require("./app/routes/user.routes.js")(app);
-require("./app/routes/tutorial.routes")(app);
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+const authRoutes = require('./routes/auth.route');
+app.use('/auth', authRoutes);
+db();
+server.listen(8000, () => console.log('server is running on port 8000'));
+
+
