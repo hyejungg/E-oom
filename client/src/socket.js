c424e2f5146d2a21505d774ee14f166be843fba9
ahrimy <dlsdjdkfla@gmail.com>
2020-09-21 01:18:26 +0900

    replace url to env

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,5 @@
 import io from 'socket.io-client';
-const sockets = io('http://localhost:8080', { autoConnect: true });
+
+const sockets = io(process.env.REACT_APP_SERVER_URL, { autoConnect: true });
 // const sockets = io('/', { autoConnect: true });
 export default sockets;
